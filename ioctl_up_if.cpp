b09575a279423cd470d4cb229d9185957202eee7
yuebaixiao <wsyueshuai@163.com>
2018-10-16 07:26:16 +0800

    down_up

---
+++ ioctl_up_if.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+
+int main(){
+  int fd;
+  ifreq ifr;
+
+  fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  strncpy(ifr.ifr_name, "enp0s3", IFNAMSIZ - 1);
+
+  //get current status
+  if(ioctl(fd, SIOCGIFFLAGS, &ifr) != 0){
+    perror("ioctl");
+    return 1;
+  }
+
+  //let net work up
+  ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
+  //change status
+  if(ioctl(fd, SIOCSIFFLAGS, &ifr) != 0){
+    perror("ioctl");
+    return 1;
+  }
+  close(fd);
+  return 0;
+
+}
